319f4a52e64b1a90ebc52bad4c3e75fafccee88b
Julio <julio.collazo1@gmail.com>
2024-05-11 10:24:24 -0400

    added tradingview

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from navigation import landing, dashboard_yf, chart
 
 # Streamlit pages
 st.set_page_config(layout = 'wide')
-st.sidebar.image(
-"https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
-width=50)
+
 
 pages = {
-    '🏠 Main Page': landing.pageI,
-    '📈 SPX Analytics': chart.pageIII
+    'Home': landing.pageI,
+    'Chart': chart.pageIII
 }
 
 selected_page = st.sidebar.radio("Navigation", pages.keys())
--- navigation/chart.py
+++ navigation/chart.py
@@ -1,110 +1,181 @@
 import streamlit as st
-import pandas as pd
+from streamlit_lightweight_charts import renderLightweightCharts
+
+import json
 import numpy as np
 import yfinance as yf
-from datetime import date, timedelta
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import pandas as pd
+import pandas_ta as ta
 
+COLOR_BULL = 'rgba(38,166,154,0.9)' # #26a69a
+COLOR_BEAR = 'rgba(239,83,80,0.9)'  # #ef5350
+
+#Change in future to tradier or Schwab
+def loadChart(ticker):
+    df = yf.Ticker(ticker).history(period="100d")[['Open', 'High', 'Low', 'Close', 'Volume']]
+    return df
 
 def pageIII():
+    # Request historic pricing data via finance.yahoo.com API
 
-    st.title('SPX Realtime Trade', anchor = "title")
-    # Sidebar
-    tickers = ('BTC', 'ETH', 'SOL', 'ADA', 'DOT', 'MATIC', 'EGLD', 'DOGE', 'XRP', 'BNB')
-    coin = st.sidebar.selectbox('Pick a coin from the list', tickers)
+    tickers = ("^SPX","SPY","QQQ","TSLA","AAPL","META","HD")
+    ticker = st.sidebar.selectbox('Pick a ticker from the list', tickers)
 
-    # Page
-    col1, col2 = st.columns([1, 5])
-    coin_image = f'img/{coin.lower()}.png'
-    col1.header(f'{coin}/USD')
-    col2.image(coin_image, width = 60)
-
-    # Metrics
-    col1, col2, col3 = st.columns([2, 2, 2])
-    info = get_market(coin)
-    price_difference_24h = (info['price'] - info['priceHigh24h'])/info['price'] * 100
-    col1.metric('Price', f'{info["price"]:,}', f'{round(price_difference_24h,2)}%')
-    col2.metric('24h High', f'{info["priceHigh24h"]:,}')
-    col3.metric('24h Low', f'{info["priceLow24h"]:,}')
-
-    st.metric('24h Volume', f'{info["volumeUsd24h"]:,}')
-
-    # Check periods
-
-    # The code has been simplified by using a dictionary to store the resolution options and default values 
-    # for each filter option, instead of having multiple if conditionals for each option.
-
-    check = st.radio('Filter', ['1D', '5D', '1M', '3M', '6M', '1Y', '2Y', 'All', 'None'], horizontal=True, index=8)
-
-    resolution_options = {
-        '1D': (["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1d"], "30m"),
-        '5D': (["30m", "60m", "90m", "1d"], "30m"),
-        '1M': (["90m", "1d", "5d", "1wk", "1mo"], "1d"),
-        '3M': (["1d", "5d", "1wk", "1mo"], "1d"),
-        '6M': (["1d", "5d", "1wk", "1mo", "3mo"], "1d"),
-        '1Y': (["1d", "5d", "1wk", "1mo", "3mo"], "1d"),
-        '2Y': (["1d", "5d", "1wk", "1mo", "3mo"], "1d")
-    }
-
-    if check == 'None':
-        start_date = st.sidebar.date_input('Start Date', value=pd.to_datetime('2022-01-01'), key='dstart_date')
-        end_date = st.sidebar.date_input('End Date', value=pd.to_datetime('now'), key='dend_date')
-        resolution = st.select_slider('Resolution', options=["1d", "5d", "1mo"], value="1d", key='Nresolution')
-        coin_df = get_historical(coin, start_date, end_date, interval=resolution)
-    elif check == 'All':
-        back_days = date.today() - timedelta(days=1095)  # 3 years
-        start_date = pd.to_datetime(back_days)
-        end_date = pd.to_datetime('now')
-        resolution = st.select_slider('Resolution', options=["1d", "5d", "1wk", "1mo", "3mo"], value="1d",
-                                    key='allrresolution')
-        coin_df = get_historical(coin, start_date=start_date, end_date=end_date, interval=resolution)
-    else:
-        options, default_value = resolution_options[check]
-        period = check.lower()
-        resolution = st.select_slider('Resolution', options=options, value=default_value,
-                                    key=f'{period}resolution')
-        coin_df = get_historical(coin, period=period, start_date=None, end_date=None, interval=resolution)
-
-    # Moving average - 30weeks
-    coin_df['30wma'] = coin_df['Close'].rolling(30).mean()
-    variance = round(np.var(coin_df['Close']),3)
+    df = loadChart(ticker)
     
-    # Candle and volume chart
-    fig = make_subplots(rows = 2, cols = 1, shared_xaxes = True, vertical_spacing = 0.1, row_heights = [100,30])
-    fig.add_trace(
-        go.Candlestick(x = coin_df['Date'],
-                        open = coin_df['Open'], high = coin_df['High'],
-                        low = coin_df['Low'], close = coin_df['Close'],
-                        name = 'Candlestick',
-                        ), row = 1, col = 1
-    )
+   
+    # Some data wrangling to match required format
+    df = df.reset_index()
+    df.columns = ['time','open','high','low','close','volume']                  # rename columns
+    df['time'] = df['time'].dt.strftime('%Y-%m-%d')                             # Date to string
+    df['color'] = np.where(  df['open'] > df['close'], COLOR_BEAR, COLOR_BULL)  # bull or bear
+    #df.ta.macd(close='close', fast=6, slow=12, signal=5, append=True)           # calculate macd
 
-    fig.update_layout(xaxis_rangeslider_visible = False)
+    # export to JSON format
+    candles = json.loads(df.to_json(orient = "records"))
+    volume = json.loads(df.rename(columns={"volume": "value",}).to_json(orient = "records"))
+    #macd_fast = json.loads(df.rename(columns={"MACDh_6_12_5": "value"}).to_json(orient = "records"))
+    #macd_slow = json.loads(df.rename(columns={"MACDs_6_12_5": "value"}).to_json(orient = "records"))
+    #df['color'] = np.where(  df['MACD_6_12_5'] > 0, COLOR_BULL, COLOR_BEAR)  # MACD histogram color
+    #macd_hist = json.loads(df.rename(columns={"MACD_6_12_5": "value"}).to_json(orient = "records"))
 
-    fig.add_trace(
-        go.Scatter(
-            x = coin_df['Date'],
-            y = coin_df['30wma'],
-            line = dict(color = '#e0e0e0', width = 2, dash = 'dot'),
-            name = "30-week MA"
-        ), row = 1, col = 1
-    )
 
-    # Bar chart https://plotly.com/python-api-reference/generated/plotly.graph_objects.bar.html#plotly.graph_objects.bar.Marker
-    fig.add_trace(
-        go.Bar(
-            x = coin_df['Date'],
-            y = coin_df['Volume'],
-            marker = dict(color = coin_df['Volume'], colorscale = 'aggrnyl_r'),
-            name = 'Volume'
-        ), row = 2, col = 1
-    )
-    fig['layout']['xaxis2']['title'] = 'Date'
-    fig['layout']['yaxis']['title'] = 'Price'
-    fig['layout']['yaxis2']['title'] = 'Volume'
-    st.plotly_chart(fig, use_container_width = True)
+    chartMultipaneOptions = [
+        {
+            "width": 950,
+            "height": 600,
+            "layout": {
+                "background": {
+                    "type": "solid",
+                    "color": 'white'
+                },
+                "textColor": "black"
+            },
+            "grid": {
+                "vertLines": {
+                    "color": "rgba(197, 203, 206, 0.5)"
+                    },
+                "horzLines": {
+                    "color": "rgba(197, 203, 206, 0.5)"
+                }
+            },
+            "crosshair": {
+                "mode": 0
+            },
+            "priceScale": {
+                "borderColor": "rgba(197, 203, 206, 0.8)"
+            },
+            "timeScale": {
+                "borderColor": "rgba(197, 203, 206, 0.8)",
+                "barSpacing": 15
+            },
+            "watermark": {
+                "visible": True,
+                "fontSize": 48,
+                "horzAlign": 'center',
+                "vertAlign": 'center',
+                "color": 'rgba(171, 71, 188, 0.3)',
+                "text": ticker,
+            }
+        },
+        {
+            "width": 1000,
+            "height": 200,
+            "layout": {
+                "background": {
+                    "type": 'solid',
+                    "color": 'transparent'
+                },
+                "textColor": 'black',
+            },
+            "grid": {
+                "vertLines": {
+                    "color": 'rgba(42, 46, 57, 0)',
+                },
+                "horzLines": {
+                    "color": 'rgba(42, 46, 57, 0.6)',
+                }
+            },
+            "timeScale": {
+                "visible": False,
+            },
+            "watermark": {
+                "visible": True,
+                "fontSize": 18,
+                "horzAlign": 'left',
+                "vertAlign": 'top',
+                "color": 'rgba(171, 71, 188, 0.7)',
+                "text": 'Volume',
+            }
+        },
+        {
+            "width": 1280,
+            "height": 800,
+            "layout": {
+                "background": {
+                    "type": "solid",
+                    "color": 'white'
+                },
+                "textColor": "black"
+            },
+            "timeScale": {
+                "visible": False,
+            },
+            "watermark": {
+                "visible": True,
+                "fontSize": 18,
+                "horzAlign": 'left',
+                "vertAlign": 'center',
+                "color": 'rgba(171, 71, 188, 0.7)',
+                "text": 'MACD',
+            }
+        }
+    ]
 
-    # Show data
-    if st.checkbox('Show data'):
-        st.dataframe(coin_df)
+    seriesCandlestickChart = [
+        {
+            "type": 'Candlestick',
+            "data": candles,
+            "options": {
+                "upColor": COLOR_BULL,
+                "downColor": COLOR_BEAR,
+                "borderVisible": False,
+                "wickUpColor": COLOR_BULL,
+                "wickDownColor": COLOR_BEAR
+            }
+        }
+    ]
+
+    seriesVolumeChart = [
+        {
+            "type": 'Histogram',
+            "data": volume,
+            "options": {
+                "priceFormat": {
+                    "type": 'volume',
+                },
+                "priceScaleId": "" # set as an overlay setting,
+            },
+            "priceScale": {
+                "scaleMargins": {
+                    "top": 0,
+                    "bottom": 0,
+                },
+                "alignLabels": False
+            }
+        }
+    ]
+
+
+    st.subheader("Reaper View")
+
+    renderLightweightCharts([
+        {
+            "chart": chartMultipaneOptions[0],
+            "series": seriesCandlestickChart
+        },
+        {
+            "chart": chartMultipaneOptions[1],
+            "series": seriesVolumeChart
+        }
+    ], 'multipane')
